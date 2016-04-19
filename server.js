9db48d96f6cf05381ae5c69989c3c52aebeb5969
Liebergen <martijn.van.liebergen@hpe.com>
2016-04-19 15:45:54 +0200

    added app insights

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
 var express = require('express');
 var app = express();
 var server = require('http').createServer(app);
+var AppInsights = require('applicationinsights');
+var appInsights = new AppInsights(
+ /* configuration can optionally be passed here instead of the environment variable, example:
+ {
+ instrumentationKey: "<guid>"
+ }
+ */
+);
 var Game = require('./game.js');
 var routes = require('./routes/routes.js');
 var players = { };
